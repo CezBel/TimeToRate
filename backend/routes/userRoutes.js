81ed38461173c46cb7f8ba3fc294eea3fa3e8aea
cezar <Tzezar32@gmail.com>
2024-02-19 17:35:27 +0200

    added jwt token as  httpOnly cookie

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import { 
+  loginUser, 
+  registerUser, 
+  logoutUser, 
+  getUserProfile, 
+  updateUserProfile, 
+  getUsers, 
+  getUserById,
+  deleteUser, 
+  updateUser  
+} from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
+const router = express.Router();
+
+router.post('/login', loginUser);
+router.post('/register', registerUser);
+router.post('/logout', protect, logoutUser);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+
+// Admin Routes
+router.get('/', protect, admin, getUsers);
+router.route('/:id').get(protect, admin, getUserById).put(protect, admin, updateUser).delete(protect, admin, deleteUser);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+dotenv.config();
+import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
-dotenv.config();
 import businessRoutes from './routes/businessRoutes.js';
+import usersRoutes from './routes/userRoutes.js';
 const PORT = process.env.PORT;
 
 connectDB();
 
 const app = express();
 
+app.use(express.json());
+
+app.use(cookieParser());
+
 app.use('/api/businesses', businessRoutes);
+app.use('/api/users', usersRoutes);
 
 // errorHandler
 app.use(notFound);
